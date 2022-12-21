22be4cf0813fa89b32c6e7b6d5f8df8ec7957615
David Crosby <dave@dafyddcrosby.com>
2022-12-21 07:57:28 -0700

    Use customize

--- init/calendar.el
+++ init/calendar.el
@@ -1,9 +1,9 @@
 (use-package calendar
   :ensure nil
+  :custom
+  (calendar-latitude 51.0453)
+  (calendar-longitude -114.058)
+  (calendar-location-name "Calgary, AB")
   :config
   (calendar-set-date-style 'iso)
-  (setq
-     calendar-latitude 51.0453
-     calendar-longitude -114.058
-     calendar-location-name "Calgary, AB")
   )
--- init/evil.el
+++ init/evil.el
@@ -1,23 +1,17 @@
 (use-package evil
   :ensure t
-  :init
-  (setq evil-want-C-i-jump nil ; Fix TAB in emacs
-        evil-undo-system 'undo-redo) ; Emacs 28+
+  :custom
+  (evil-want-C-i-jump nil "Fix TAB in emacs")
+  (evil-undo-system 'undo-redo "Note: requires Emacs 28+")
   :config
   (evil-mode 1)
+  (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
+  (evil-define-key 'normal org-mode-map (kbd "RET") #'org-open-at-point)
+
+  (evil-define-key 'motion Info-mode-map (kbd "RET") #'Info-follow-nearest-node)
+  (evil-define-key 'motion Info-mode-map (kbd "[") #'Info-backward-node)
+  (evil-define-key 'motion Info-mode-map (kbd "]") #'Info-forward-node)
+
+  (evil-define-key nil 'global (kbd "C-S-v") #'yank)
+  (evil-ex-define-cmd "q" #'kill-this-buffer)
   )
-
-(evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
-(evil-define-key 'normal org-mode-map (kbd "RET") #'org-open-at-point)
-
-(evil-define-key 'motion Info-mode-map (kbd "RET") #'Info-follow-nearest-node)
-(evil-define-key 'motion Info-mode-map (kbd "[") #'Info-backward-node)
-(evil-define-key 'motion Info-mode-map (kbd "]") #'Info-forward-node)
-
-(evil-define-key nil 'global (kbd "C-S-v") #'yank)
-
-;; (require 'org-evil)
-
-;; Evil numbers
-;(define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
-;(define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
