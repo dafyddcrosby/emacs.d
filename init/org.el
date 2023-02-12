5bfd4fa37287c350c4ead01f823f3de788b47730
David Crosby <dave@dafyddcrosby.com>
2023-02-12 10:35:52 -0700

    org mode tweaks

--- init/org.el
+++ init/org.el
@@ -1,6 +1,6 @@
 (use-package org
   :custom
-  (org-agenda-dim-blocked-tasks nil "Speed up agenda")
+  (org-agenda-dim-blocked-tasks 'invisible)
   (org-agenda-show-all-dates t "show empty dates")
   (org-agenda-todo-ignore-scheduled 'future "ignore SCHEDULED until day arrives")
   (org-agenda-use-tag-inheritance nil "Speed up agenda")
@@ -9,12 +9,14 @@
   (org-clock-persist t "Save running clocks between Emacs sessions")
   (org-clock-persist-query-resume nil "Ask before resuming clock")
   (org-ellipsis "⤵")
+  (org-enforce-todo-dependencies t)
   (org-export-with-special-strings nil)
   (org-habit-scheduled-past-days 10000 "Continue seeing undone habits")
   (org-habit-show-habits-only-for-today nil "I like to see upcoming habits")
   (org-hierarchical-todo-statistics nil "Consider all tree entries")
   (org-icalendar-include-todo t "Add unfinished VTODO to icalendar exports")
   (org-icalendar-store-UID t "To prevent duplicate TODOs, adds ID properties to each node")
+  (org-log-into-drawer t)
   (org-refile-use-outline-path 'file "Use file name to determine refile path")
   (org-refile-targets '((nil :maxlevel . 5) (org-agenda-files :maxlevel . 5)))
   (org-return-follows-link t)
@@ -34,7 +36,7 @@
    '(
      (sequence "TODO(t)" "WIP(w)" "VERIFY(v)" "|" "DONE(d)")
      (sequence "REPORT(r)" "BUG(b)" "KNOWNCAUSE(k)" "|" "FIXED(f)")
-     (sequence "|" "CANCELED(c)")
+     (sequence "|" "CANCELLED(c)")
      ))
   :bind
   (
