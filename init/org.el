874a96707a825ad49e856841ccc961449aa18b98
David Crosby <dave@dafyddcrosby.ca>
2026-02-07 19:24:19 -0700

    More org link abbrevs

--- init/org.el
+++ init/org.el
@@ -29,18 +29,25 @@
   ; https://orgmode.org/manual/Link-Abbreviations.html
   (org-link-abbrev-alist
    '(
-     ("chefresource" . "https://docs.chef.io/resources/%s/")
-     ("cookstyle"  . "https://docs.chef.io/workstation/cookstyle/%s/")
+     ("bandcamp"   . "https://%s.bandcamp.com/")
      ("duckduckgo" . "https://duckduckgo.com/?q=%s")
-     ("ghrepo"     . "https://github.com/%s")
-     ("inspecresource" . "https://docs.chef.io/inspec/resources/%s/")
      ("omap"       . "https://nominatim.openstreetmap.org/ui/search.html?q=%s&polygon=1")
+     ("patreon"    . "https://patreon.com/%s")
+     ("wp"         . "https://en.wikipedia.org/wiki/%s")
+     ("yt"         . "https://www.youtube.com/watch?v=%s")
+     ("ytchan"     . "https://www.youtube.com/c/%s")
+
+     ("emacswiki"  . "https://www.emacswiki.org/emacs/%s")
+     ("huggingface" . "https://huggingface.co/%s")
+     ("ghrepo"     . "https://github.com/%s")
      ("pypi"       . "https://pypi.org/project/%s")
      ("rubydoc"    . "https://docs.ruby-lang.org/en/master/%s.html")
      ("rubygems"   . "https://rubygems.org/gems/%s")
+
+     ("chefresource" . "https://docs.chef.io/resources/%s/")
+     ("cookstyle"  . "https://docs.chef.io/workstation/cookstyle/%s/")
+     ("inspecresource" . "https://docs.chef.io/inspec/resources/%s/")
      ("supermarket" . "https://supermarket.chef.io/cookbooks/%s")
-     ("wp"         . "https://en.wikipedia.org/wiki/%s")
-     ("yt"         . "https://www.youtube.com/watch?v=%s")
      ))
 
   (org-todo-keywords
